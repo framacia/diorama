0b94629b65ef7209391d3d6b23c9d2e2d49ca663
Francisco Macia <frankmmacia@gmail.com>
2018-10-24 23:41:16 +0100

    splash screen and shit

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,30 +7,42 @@ public class SceneController : MonoBehaviour {
 
 	private GameObject panel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		if (Input.GetKeyDown(KeyCode.Escape))
 		{
-			print("Application quitted");
-			Application.Quit();
+			MainMenu();
 		}		
 	}
 
-	public void Quit() 
+	public void MainMenu() 
 	{
-		Debug.Log("Application quitted");
-		Application.Quit();
-	}
+		Debug.Log("Back to main menu");
+        SceneManager.LoadScene(1);
+    }
 
-	public void LoadScene(string nextScene)
-	{
-		Debug.Log("Loading scene " + nextScene);
-		SceneManager.LoadScene(nextScene);
-	}
+    public void Quit()
+    {
+        Debug.Log("Game quitted");
+        Application.Quit();
+    }
 
+    public void Aerial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FirstPerson()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+   
 }
---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        SceneManager.LoadScene(1);
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(ExecuteAfterTime(10));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
